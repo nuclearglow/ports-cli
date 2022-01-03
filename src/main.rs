13ab12c1f55b930c4e35cf05472a28847e5046d0
Sven Vowe <svenvowe@gmail.com>
2022-01-03 22:59:20 +0100

    feat: working table, colorized

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #[macro_use]
 extern crate prettytable;
-use prettytable::{format, Table};
-use std::ffi::OsStr;
+
+use colored::*;
+use prettytable::{color, format, Attr, Cell, Row, Table};
 
 mod ports;
 mod processes;
@@ -13,30 +14,52 @@ fn main() {
 
     let sockets = ports::get_open_ports();
     for socket in sockets {
-        table.add_row(row![
-            l->socket.address,
-            l->socket.port,
-            l->
-                socket.process_info
+        let color;
+
+        if socket.process_info.iter().any(|info| info.is_current_user) {
+            color = color::YELLOW
+        } else {
+            color = match socket.port {
+                0..=1023 => color::RED,
+                1024..=49151 => color::BRIGHT_GREEN,
+                _ => color::GREEN,
+            }
+        }
+
+        table.add_row(Row::new(vec![
+            Cell::new(&socket.address.to_string().green()).with_style(Attr::ForegroundColor(color)),
+            Cell::new(&socket.port.to_string().red()).with_style(Attr::ForegroundColor(color)),
+            Cell::new(
+                &socket
+                    .process_info
                     .iter()
                     .map(|info| info.name.to_owned())
                     .collect::<Vec<String>>()
                     .join("\n"),
-            l->
-                socket.process_info
+            )
+            .with_style(Attr::ForegroundColor(color)),
+            Cell::new(
+                &socket
+                    .process_info
                     .iter()
                     .map(|info| info.owner.name().to_os_string().into_string().unwrap())
                     .collect::<Vec<String>>()
                     .join("\n"),
-            l->socket.process_info
+            )
+            .with_style(Attr::ForegroundColor(color)),
+            Cell::new(
+                &socket
+                    .process_info
                     .iter()
                     .map(|info| match info.cmd.chars().count() {
                         0..=40 => info.cmd.to_owned(),
-                        _ => format!("{}...", info.cmd[0..40].to_string())
+                        _ => format!("{}...", info.cmd[0..40].to_string()),
                     })
                     .collect::<Vec<String>>()
                     .join("\n"),
-        ]);
+            )
+            .with_style(Attr::ForegroundColor(color)),
+        ]));
     }
-    println!("{}", table);
+    table.printstd();
 }
